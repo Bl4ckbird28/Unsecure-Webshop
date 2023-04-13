2a3fe724121d880105461715424b17485444ae0c
TimNöth <tim.noeth@student.fhws.de>
2023-04-13 11:13:04 +0200

    edited pom.xml for @ParameterizedTest and added TestCases for 2.Submission

---
+++ test/utils/logic/UserLogicTestParameterized.java
@@ -0,0 +1,31 @@
+package utils.logic;
+
+import de.fhws.biedermann.webshop.utils.logic.UserLogic;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserLogicTestParameterized {
+
+    @ParameterizedTest
+    @CsvSource({
+            "T1.1, Start123!, Hallo123!"
+    })
+    public void testValidPasswords( String testCaseId, String oldPass, String newPass ) {
+        assertTrue( UserLogic.isValidPassword( oldPass, newPass ) );
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "T1.2, Start123!, Start123!",
+            "T1.3, Start123!, Hal1!",
+            "T1.4, Start123!, hallo123!",
+            "T1.5, Start123!, Hallonum!",
+            "T1.6, Start123!, Hallo123s"
+    })
+    public void testInvalidPasswords( String testCaseId, String oldPass, String newPass ) {
+        assertFalse( UserLogic.isValidPassword( oldPass, newPass ) );
+    }
+
+}

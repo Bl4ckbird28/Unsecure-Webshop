9f542c17d438d78f5af5eb37505a69e70326fc2a
TimNöth <tim.noeth@student.fhws.de>
2023-05-16 14:47:19 +0200

    deactivated AlwaysFailTest

--- test/utils/logic/AlwaysFailTest.java
+++ test/utils/logic/AlwaysFailTest.java
@@ -2,11 +2,9 @@ package utils.logic;
 
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class AlwaysFailTest {
     @Test
     public void test_always_fails () {
-        true;
+
     }
 }

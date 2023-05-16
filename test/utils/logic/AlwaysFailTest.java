09281689614d748520bf1bee21d458b9bf98bcc3
TimNöth <tim.noeth@student.fhws.de>
2023-05-16 14:23:37 +0200

    added Target-folder

---
+++ test/utils/logic/AlwaysFailTest.java
@@ -0,0 +1,12 @@
+package utils.logic;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AlwaysFailTest {
+    @Test
+    public void test_always_fails () {
+        fail();
+    }
+}
